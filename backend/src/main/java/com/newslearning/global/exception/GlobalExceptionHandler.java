25f144cd572f845e47246f89bee3b4847cda3d90
Hyoseok Seo <seohyoseok0408@gmail.com>
2025-07-24 01:46:53 +0900

    feat: 전역 예외 처리 및 공통 응답 구조 도입

---
+++ backend/src/main/java/com/newslearning/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.newslearning.global.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
