b97d597badc87bca2ab1130e71adda8662c73bb5
Hyoseok Seo <seohyoseok0408@gmail.com>
2025-07-23 22:49:14 +0900

    feat: 초기 프로젝트 세팅 (backend, frontend, gitignore 등)

---
+++ backend/src/main/java/com/newslearning/NewsLearningApplication.java
@@ -0,0 +1,13 @@
+package com.newslearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsLearningApplication.class, args);
+	}
+
+}
